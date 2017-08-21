627e234086f7f6ce331be4a43774185129f59a4e
滕昕帅 <txs19970905@outlook.com>
2017-08-21 21:14:51 +0800

    阴影txs

---
+++ Hotel/GetHotels/Controller/SignInViewController.m
@@ -0,0 +1,37 @@
+//
+//  SignInViewController.m
+//  GetHotels
+//
+//  Created by admin on 2017/8/21.
+//  Copyright © 2017年 Yixin studio. All rights reserved.
+//
+
+#import "SignInViewController.h"
+
+@interface SignInViewController ()
+
+@end
+
+@implementation SignInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
