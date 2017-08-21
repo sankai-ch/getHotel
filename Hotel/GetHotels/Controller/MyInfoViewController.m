34bf6cc454bc4de7f47db23872942da18d9df210
王俊 <820989774@qq.com>
2017-08-21 20:18:28 +0800

    我的

---
+++ Hotel/GetHotels/Controller/MyInfoViewController.m
@@ -0,0 +1,37 @@
+//
+//  MyInfoViewController.m
+//  GetHotels
+//
+//  Created by admin1 on 2017/8/21.
+//  Copyright © 2017年 Yixin studio. All rights reserved.
+//
+
+#import "MyInfoViewController.h"
+
+@interface MyInfoViewController ()
+
+@end
+
+@implementation MyInfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
