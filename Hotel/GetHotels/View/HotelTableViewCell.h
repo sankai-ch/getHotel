77bb0419cb487ee6a09f732903246cbafe3a6bf0
刘杨洋 <757273401@qq.com>
2017-08-21 21:23:27 +0800

    getHotel界面

---
+++ Hotel/GetHotels/View/HotelTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HotelTableViewCell.h
+//  GetHotels
+//
+//  Created by ll on 2017/8/21.
+//  Copyright © 2017年 Yixin studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotelTableViewCell : UITableViewCell
+
+@end
