dc9feb80b2836d9db4cea63990e1434885d93337
龙聪 <2987113417@qq.com>
2017-08-22 08:53:58 +0800

    detail

---
+++ Hotel/GetHotels/View/DetailViewController.m
@@ -0,0 +1,53 @@
+//
+//  DetailViewController.m
+//  GetHotels
+//
+//  Created by admin1 on 2017/8/21.
+//  Copyright © 2017年 Yixin studio. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self setNavigationItem];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+//设置导航栏样式
+-(void)setNavigationItem{
+    [self.navigationController.navigationBar setBarTintColor:[UIColor blueColor]];
+    //实例化一个button
+    UIButton *leftBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    //设置button的位置大小
+    leftBtn.frame = CGRectMake(0, 0, 20, 20);
+    //设置背景图片
+    [leftBtn setBackgroundImage:[UIImage imageNamed:@"返回"] forState:UIControlStateNormal];
+    //给按钮添加事件
+    [leftBtn addTarget:self action:@selector(leftButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:leftBtn];
+}
+-(void)leftButtonAction:(UIButton *)sender{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
