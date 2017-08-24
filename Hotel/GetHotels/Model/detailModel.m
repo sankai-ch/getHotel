9ff6403c347722c658d7d04f7be83a17a65fdc6b
龙聪 <2987113417@qq.com>
2017-08-24 17:39:29 +0800

    加了sd

---
+++ Hotel/GetHotels/Model/detailModel.m
@@ -0,0 +1,23 @@
+//
+//  detailModel.m
+//  GetHotels
+//
+//  Created by admin1 on 2017/8/24.
+//  Copyright © 2017年 Yixin studio. All rights reserved.
+//
+
+#import "detailModel.h"
+
+@implementation detailModel
+-(instancetype)initWithDict:(NSDictionary *)dict{
+    self = [super init];
+    if(self){
+        self.hotels = [Utilities nullAndNilCheck:dict[@"hotel_name"] replaceBy:@"无"];
+     self.address = [Utilities nullAndNilCheck:dict[@"hotel_address"] replaceBy:@"无"];
+         self.image = [Utilities nullAndNilCheck:dict[@"hotel_img"] replaceBy:@"无"];
+        self.price=[[Utilities nullAndNilCheck:dict[@"price"] replaceBy:0]integerValue];
+        
+    }
+    return self;
+}
+@end
