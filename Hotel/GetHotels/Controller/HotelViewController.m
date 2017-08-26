fbe8dab6bd5032a3c66dfd8bbd836044e0526f24
刘杨洋 <757273401@qq.com>
2017-08-26 16:40:33 +0800

    首页跳转

--- Hotel/GetHotels/Controller/HotelViewController.m
+++ Hotel/GetHotels/Controller/HotelViewController.m
@@ -583,13 +583,13 @@
         return;
     }
     AAndHModel *hotelID = _hotelArr[indexPath.row];
-    DetailViewController *detailVC = [Utilities getStoryboardInstance:@"Deatil" byIdentity:@"reservation"];
-    //UINavigationController *nc = [[UINavigationController alloc]initWithRootViewController:detailVC];
+//    DetailViewController *detailVC = [Utilities getStoryboardInstance:@"Deatil" byIdentity:@"reservation"];
+    UINavigationController *nc = [Utilities getStoryboardInstance:@"Deatil" byIdentity:@"reservation"];
     //[self presentViewController:nc animated:YES completion:nil];
-    detailVC.hotelid = [NSString stringWithFormat:@"%ld",(long)hotelID.hotelId];
-    NSLog(@"%@",detailVC.hotelid);
+    //detailVC.hotelid = [NSString stringWithFormat:@"%ld",(long)hotelID.hotelId];
+    //NSLog(@"%@",detailVC.hotelid);
     
-    [self.navigationController pushViewController:detailVC animated:YES];
+    [self.navigationController pushViewController:nc animated:YES];
 }
 
 
