9c0d318df8fc25db7426a9b569ff505bde3e226d
王俊 <820989774@qq.com>
2017-09-14 15:21:48 +0800

    报价列表支付页面99%

---
+++ Hotel/GetHotels/Model/QuoteModel.m
@@ -0,0 +1,29 @@
+//
+//  QuoteModel.m
+//  GetHotels
+//
+//  Created by admin1 on 2017/9/14.
+//  Copyright © 2017年 Yixin studio. All rights reserved.
+//
+
+#import "QuoteModel.h"
+
+@implementation QuoteModel
+
+- (instancetype)initWithDict:(NSDictionary *)dict
+{
+    self = [super init];
+    if (self) {
+        self.aviation_cabin = [Utilities nullAndNilCheck:dict[@"aviation_cabin"] replaceBy:@""];
+        self.aviation_company = [Utilities nullAndNilCheck:dict[@"aviation_company"] replaceBy:@""];
+        self.departure = [Utilities nullAndNilCheck:dict[@"departure"] replaceBy:@"暂无"];
+        self.destination = [Utilities nullAndNilCheck:dict[@"destination"] replaceBy:@"无"];
+        self.flight_no = [Utilities nullAndNilCheck:dict[@"flight_no"] replaceBy:@"1"];
+        self.final_price = [[Utilities nullAndNilCheck:dict[@"final_price"] replaceBy:0 ]integerValue];
+        self.in_time_str = [Utilities nullAndNilCheck:dict[@"in_time_str"] replaceBy:@"1"];
+        self.out_time_str = [Utilities nullAndNilCheck:dict[@"out_time_str"] replaceBy:@""];
+            }
+    return self;
+}
+
+@end
